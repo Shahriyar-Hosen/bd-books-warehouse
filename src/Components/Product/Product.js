cf1abf023103dcc6b3e194c113a8520aed9b294f
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-05-01 02:00:00 +0600

    card full setup & info add

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,24 +1,47 @@
 import React from "react";
 import { Card, Col } from "react-bootstrap";
+import "./Product.css";
 
-const Product = ({product}) => {
-    const {image} = product;
+const Product = ({ product }) => {
+  const { img, name, price, description, quantity, supplier, sold, category } =
+    product;
   return (
-    <div>
-      <Col>
-            <Card>
-              <Card.Img style={{width: "300px"}} className="mx-auto pt-1" variant="top" src={image} />
-              <Card.Body>
-                <Card.Title>Card title</Card.Title>
-                <Card.Text>
-                  This is a longer card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-    </div>
+    <Col className="">
+      <Card className="cart mx-auto" style={{ width: "350px" }}>
+        <Card.Img className=" d-fluid" variant="top" src={img} />
+        <Card.Body className="mx-3">
+          <Card.Title className="my-3 fw-bold fs-4 text-center">
+            {name}
+          </Card.Title>
+          <div className="d-flex justify-content-around">
+            <div>
+              <Card.Text className="my-1 fw-bold text-war text-success">
+                Price: {price}
+              </Card.Text>
+              <Card.Text className="my-1 fw-bold text-success">
+                Supplier: {supplier}
+              </Card.Text>
+            </div>
+            <div>
+              <Card.Text className="my-1 fw-bold text-success">
+                Quantity: {quantity}
+              </Card.Text>
+              <Card.Text className="my-1 fw-bold text-success">
+                Sold: {sold}
+              </Card.Text>
+            </div>
+          </div>
+          <Card.Text className="my-2">Category: {category}</Card.Text>
+          <Card.Text>
+            <small>
+              {description.length > 60
+                ? description.slice(0, 60) + "..."
+                : description}
+            </small>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </Col>
   );
 };
 
--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -6,9 +6,9 @@ import Product from "../Product/Product";
 const Products = () => {
   const [inventory] = useInventory([]);
   return (
-    <div>
+    <div className="container my-5">
       <h1>Product: {inventory.length}</h1>
-      <div className="container">
+      <div>
         <Row xs={1} md={3} className="g-4">
           {inventory.slice(0, 6).map((items) => (
             <Product key={items._id} product={items}></Product>
