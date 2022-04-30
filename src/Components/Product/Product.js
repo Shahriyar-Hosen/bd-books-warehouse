8703e7b8428b2b648a64284e24872c1bc356ee4e
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-05-01 04:30:30 +0600

    cart bootstrap style add

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,20 +1,19 @@
 import React from "react";
 import { Card, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import "./Product.css";
 
 const Product = ({ product }) => {
-  const {_id, img, name, price, description, quantity, supplier, sold, category } =
+  const {_id, img, name, price, description, quantity, supplier, sold } =
     product;
   return (
     <Col>
-      <Card className="cart-custom mx-auto" style={{ width: "350px" }}>
-        <Card.Img className=" d-fluid p-1 rounded" variant="top" src={img} />
+      <Card className="cart-custom bg-info bg-gradient bg-opacity-10 mx-auto border border-info " style={{ width: "350px", borderRadius:"7px" }}>
+        <Card.Img style={{ borderRadius:"10px" }} className=" d-fluid p-1 rounded-5" variant="top" src={img} />
         <Card.Body className="mx-3">
           <Card.Title className="my-3 fw-bold fs-4 text-center">
             {name}
           </Card.Title>
-          <div className="d-flex justify-content-around">
+          <div className="d-flex justify-content-around mb-2">
             <div>
               <Card.Text className="my-1 fw-bold text-war text-success">
                 Price: {price}
@@ -32,7 +31,6 @@ const Product = ({ product }) => {
               </Card.Text>
             </div>
           </div>
-          <Card.Text className="my-2">Category: {category}</Card.Text>
           <Card.Text>
             <small>
               {description.length > 80
@@ -41,7 +39,7 @@ const Product = ({ product }) => {
             </small>
           </Card.Text>
         </Card.Body>
-        <Card.Footer className="border-0 bg-c-footer pb-0 mx-auto d-flex">
+        <Card.Footer className="border-0 bg-c-footer bg-info bg-gradient bg-opacity-10 pb-0 w-100 d-flex justify-content-center">
           <Link to={`update${_id}`}>
             {" "}
             <button className="btn-custom px-5">UPDATE</button>
--- src/Components/UpdateItems/UpdateItems.js
+++ src/Components/UpdateItems/UpdateItems.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React from "react";
+import { useParams } from "react-router-dom";
 
 const UpdateItems = () => {
-    const {id} = useParams();
-    return (
-        <div>
-            <p>This is Update Items ID: {id}</p>
-        </div>
-    );
+  const { id } = useParams();
+  // category
+  //   <Card.Text className="my-2">Category: {category}</Card.Text>
+  return (
+    <div>
+      <p>This is Update Items ID: {id}</p>
+    </div>
+  );
 };
 
 export default UpdateItems;
