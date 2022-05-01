ef61172ad19a76391922fdaa545f24cba5ac1875
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-05-01 12:25:22 +0600

    UpdateItems cart style & footer btn add

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -3,17 +3,31 @@ import { Card, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 const Product = ({ product }) => {
-  const {_id, img, name, price, description, quantity, supplier, sold } =
+  const { _id, img, name, price, description, quantity, supplier, sold } =
     product;
   return (
     <Col>
-      <Card className="cart-custom bg-info bg-gradient bg-opacity-10 mx-auto border border-info " style={{ width: "350px", borderRadius:"7px" }}>
-        <Card.Img style={{ borderRadius:"10px" }} className=" d-fluid p-1 rounded-5" variant="top" src={img} />
+      <Card
+        className="cart-custom bg-info bg-gradient bg-opacity-10 mx-auto border border-info shadow-lg"
+        style={{ width: "350px", borderRadius: "7px" }}
+      >
+        <Card.Img
+          style={{ borderRadius: "10px" }}
+          className=" d-fluid p-1 rounded-5"
+          variant="top"
+          src={img}
+        />
         <Card.Body className="mx-3">
-          <Card.Title  style={{color:"navy"}} className="my-3 fw-bold fs-4 text-center">
+          <Card.Title
+            style={{ color: "navy" }}
+            className="my-3 fw-bold fs-4 text-center"
+          >
             {name}
           </Card.Title>
-          <div style={{color:"rgb(2, 2, 175)"}} className="d-flex justify-content-around mb-2  text-">
+          <div
+            style={{ color: "rgb(2, 2, 175)" }}
+            className="d-flex justify-content-around mb-2  text-"
+          >
             <div>
               <Card.Text className="my-1 fw-bold text-war ">
                 Price: {price}
@@ -26,9 +40,7 @@ const Product = ({ product }) => {
               <Card.Text className="my-1 fw-bold">
                 Quantity: {quantity}
               </Card.Text>
-              <Card.Text className="my-1 fw-bold">
-                Sold: {sold}
-              </Card.Text>
+              <Card.Text className="my-1 fw-bold">Sold: {sold}</Card.Text>
             </div>
           </div>
           <Card.Text>
--- src/Components/UpdateItems/UpdateItems.js
+++ src/Components/UpdateItems/UpdateItems.js
@@ -1,49 +1,86 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Card, CardGroup } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 
 const UpdateItems = () => {
   const { id } = useParams();
-  const [product, setProducts] = useState([])
-  const {_id, img, name, price, description, quantity, supplier, sold, category } =
-    product;
+  const [product, setProducts] = useState([]);
+  const {
+    _id,
+    img,
+    name,
+    price,
+    description,
+    quantity,
+    supplier,
+    sold,
+    category,
+  } = product;
+  useEffect(() => {
+    const url = `https://quiet-sierra-51150.herokuapp.com/inventory/${id}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, [id]);
+
   return (
-    <div className="container">
-      <p>This is Update Items ID: {id}</p>
-      <CardGroup className="w-75 mx-auto py-5">
-        <Card>
-          <Card.Img variant="top" src={img} />
+    <div className="container w-100 ">
+      <CardGroup className="w-75 w-md-50 mx-auto py-5">
+        <Card
+          className=" shadow-lg bg-info bg-gradient bg-opacity-10 mx-auto border border-info"
+          style={{ borderRadius: "7px" }}
+        >
+          <Card.Img
+            style={{ maxWidth: "400px", borderRadius: "10px" }}
+            className="w-50 w-md-25 mx-auto"
+            variant="top"
+            src={img}
+          />
           <Card.Body>
-            <Card.Title  style={{color:"navy"}} className="my-3 fw-bold fs-4 text-center">Card title{name}</Card.Title>
-            <div style={{color:"rgb(2, 2, 175)"}} className="d-flex justify-content-around mb-2  text-">
-            <div>
-              <Card.Text className="my-1 fw-bold text-war ">
-                Price: {price}
-              </Card.Text>
-              <Card.Text className="my-1 fw-bold">
-                Supplier: {supplier}
-              </Card.Text>
+            <Card.Title
+              style={{ color: "navy" }}
+              className="my-3 fw-bold fs-4 text-center"
+            >
+              {name}
+            </Card.Title>
+            <div
+              style={{
+                color: "rgb(2, 2, 175)",
+                width: "80%",
+                borderRadius: "10px",
+                backgroundColor: "rgb(232, 255, 187)",
+              }}
+              className=" mx-auto d-flex justify-content-around shadow-sm p-2 mb-3"
+            >
+              <div>
+                <Card.Text className="my-1 fw-bold text-war ">
+                  Price: {price}
+                </Card.Text>
+                <Card.Text className="my-1 fw-bold">
+                  Supplier: {supplier}
+                </Card.Text>
+              </div>
+              <div>
+                <Card.Text className="my-1 fw-bold">
+                  Quantity: {quantity}
+                </Card.Text>
+                <Card.Text className="my-1 fw-bold">Sold: {sold}</Card.Text>
+              </div>
             </div>
-            <div>
-              <Card.Text className="my-1 fw-bold">
-                Quantity: {quantity}
-              </Card.Text>
-              <Card.Text className="my-1 fw-bold">
-                Sold: {sold}
-              </Card.Text>
-            </div>
-          </div>
-          <Card.Text className="my-2">Category: {category}</Card.Text>
-          <Card.Text>
-            <small>
-              {description?.length > 80
-                ? description.slice(0, 78) + "..."
-                : description}
-            </small>
-          </Card.Text>
+            <Card.Text className="my-2 text-center">
+              Category: {category}
+            </Card.Text>
+            <Card.Text>
+              <small>
+                {description?.length > 250
+                  ? description.slice(0, 250) + "..."
+                  : description}
+              </small>
+            </Card.Text>
           </Card.Body>
-          <Card.Footer>
-            <small className="text-">Last updated 3 mins ago</small>
+          <Card.Footer className="border-0 bg-c-footer bg-info bg-gradient bg-opacity-10 pb-0 w-100 d-flex justify-content-center">
+            <button className="btn-custom px-5 me-2">UPDATE</button>
+            <button className="btn-custom px-5 me-2">UPDATE</button>
           </Card.Footer>
         </Card>
       </CardGroup>
