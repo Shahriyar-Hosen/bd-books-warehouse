6d8c98e8454df15d14b48aa74c1875533b119c40
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-05-02 12:59:08 +0600

    header setup

--- src/Components/Sheared/Header/Header.js
+++ src/Components/Sheared/Header/Header.js
@@ -7,56 +7,61 @@ const Header = () => {
   return (
     <div>
       <Navbar bg="light" expand="lg">
-        <Container className="d-flex justify-content-between align-items-center">
-          <div>
-            <Navbar.Brand
-              as={Link}
-              to="/"
-              className="text-primary fs-3 fw-bold text font-tapestry"
+        <Container>
+          <Navbar.Brand
+            as={Link}
+            to="/"
+            className="text-primary fs-3 fw-bold text font-tapestry"
+          >
+            BD Books Warehouse
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0 ms-3"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
             >
-              BD Books Warehouse
-            </Navbar.Brand>
-          </div>
-          <div className="me-5">
-            <Navbar.Toggle aria-controls="navbarScroll" />
-            <Navbar.Collapse id="navbarScroll">
-              <Nav
-                className="me-auto my-2 my-lg-0"
-                style={{ maxHeight: "100px" }}
-                navbarScroll
+              <Nav.Link
+                as={Link}
+                to="/home"
+                className=" text-dark fw-bold fs-6"
               >
-                <Nav.Link
-                  as={Link}
-                  to="/home"
-                  className=" text-dark fw-bold fs-6"
-                >
-                  Home
-                </Nav.Link>
+                Home
+              </Nav.Link>
 
-                <Nav.Link
-                  as={Link}
-                  to="/manage-inventories"
-                  className=" text-dark fw-bold fs-6"
-                >
-                  Manage Inventories
-                </Nav.Link>
-                <Nav.Link
-                  as={Link}
-                  to="/add-item"
-                  className=" text-dark fw-bold fs-6"
-                >
-                  Add Item
-                </Nav.Link>
-                <Nav.Link
-                  as={Link}
-                  to="/my-items"
-                  className=" text-dark fw-bold fs-6"
-                >
-                  My items
-                </Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </div>
+              <Nav.Link
+                as={Link}
+                to="/manage-inventories"
+                className=" text-dark fw-bold fs-6"
+              >
+                Manage Inventories
+              </Nav.Link>
+              <Nav.Link
+                as={Link}
+                to="/add-item"
+                className=" text-dark fw-bold fs-6"
+              >
+                Add Item
+              </Nav.Link>
+              <Nav.Link
+                as={Link}
+                to="/my-items"
+                className=" text-dark fw-bold fs-6"
+              >
+                My items
+              </Nav.Link>
+            </Nav>
+            <Navbar.Text>
+              <Nav.Link
+                as={Link}
+                to="/login"
+                className=" text-dark fw-bold fs-6"
+              >
+                Login
+              </Nav.Link>
+            </Navbar.Text>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </div>
