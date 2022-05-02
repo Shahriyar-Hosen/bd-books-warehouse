2a7e842c57916aa19c3268a86ffdd0096ed96248
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-05-02 16:09:25 +0600

    Add Item successfully yeaaa

--- src/Components/AddItem/AddItem.js
+++ src/Components/AddItem/AddItem.js
@@ -2,35 +2,102 @@ import React from "react";
 import { Button, Form } from "react-bootstrap";
 
 const AddItem = () => {
+//   const items = {
+    /* name
+"Ceṣṭara jimanesiyama future era Calcium"
+
+supplier
+"Rokomari"
+
+category
+"Self-developmental"
+
+img
+"https://i.ibb.co/m4d4MTM/site-img-5.png" 
+
+description
+"So that by eating bamboo, Bedisha, frustration with life."
+
+*/
+//   };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const price = event.target.price.value;
+    const quantity = event.target.quantity.value;
+    const sold = event.target.sold.value;
+    const supplier = event.target.supplier.value;
+    const img = event.target.img.value;
+    const category = event.target.category.value;
+    const description = event.target.description.value;
+    
+    const inventory = {
+      name,
+      price: parseInt(price),
+      quantity: parseInt(quantity),
+      sold: parseInt(sold),
+      supplier,
+      img,
+      category,
+      description,
+    };
+
+    console.log(inventory);
+
+    // Create / POST Method - Add to database
+    fetch("https://quiet-sierra-51150.herokuapp.com/inventory", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(inventory),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+        event.target.reset();
+        alert("user added successfully");
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+    // ------------------------------------
+  };
+
   return (
     <div className="container">
-      <Form className="w-50 mx-auto py-5">
+      <Form className="w-50 mx-auto py-5" onSubmit={handleSubmit}>
         <h1 className="text-center" style={{ color: "navy" }}>
           Add Item
         </h1>
         <Form.Group className="mb-3">
-          <Form.Control type="text" placeholder="Name" />
+          <Form.Control type="text" name="name" placeholder="Name" />
         </Form.Group>
         <Form.Group className="mb-3">
-          <Form.Control type="number" placeholder="Price" />
+          <Form.Control type="number" placeholder="Price" name="price" />
         </Form.Group>
         <Form.Group className="mb-3">
-          <Form.Control type="number" placeholder="quantity" />
+          <Form.Control type="number" placeholder="quantity" name="quantity" />
         </Form.Group>
         <Form.Group className="mb-3">
-          <Form.Control type="number" placeholder="sold" />
+          <Form.Control type="number" placeholder="sold" name="sold" />
         </Form.Group>
         <Form.Group className="mb-3">
-          <Form.Control type="text" placeholder="supplier" />
+          <Form.Control type="text" placeholder="supplier" name="supplier" />
         </Form.Group>
         <Form.Group className="mb-3">
-          <Form.Control type="text" placeholder="img" />
+          <Form.Control type="text" placeholder="category" name="category" />
         </Form.Group>
         <Form.Group className="mb-3">
-          <Form.Control type="email" placeholder="category" />
+          <Form.Control type="text" placeholder="img" name="img" />
         </Form.Group>
         <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-          <Form.Control as="textarea" rows={3} placeholder="description" />
+          <Form.Control
+            as="textarea"
+            rows={3}
+            placeholder="description"
+            name="description"
+          />
         </Form.Group>
         <Button
           className="d-block mx-auto px-5 text-white fs-4"
