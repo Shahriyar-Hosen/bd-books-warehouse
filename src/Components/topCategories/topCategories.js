fb9e75ff48a083d22c3afe509c999f9d8523b926
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-05-06 08:28:56 +0600

    TOP CATEGORIES compo add

---
+++ src/Components/topCategories/topCategories.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const topCategories = () => {
+  return (
+    <div>
+      <h1 style={{ color: "navy" }} className="my-5 text-center fw-bold">
+        TOP CATEGORIES
+      </h1>
+    </div>
+  );
+};
+
+export default topCategories;
