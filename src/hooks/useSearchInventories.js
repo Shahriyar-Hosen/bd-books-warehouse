41b6ddb93739f94e4622a3082571032423219913
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-05-08 15:50:50 +0600

    if 401 & 403 than returan signout &  login

--- src/hooks/useSearchInventories.js
+++ src/hooks/useSearchInventories.js
@@ -1,7 +1,11 @@
+import { signOut } from "firebase/auth";
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import auth from "../firebase.init";
 const axios = require("axios");
 const useSearchInventories = (email) => {
   const [inventory, setInventory] = useState([]);
+  const navigate = useNavigate();
 
   // Read / Get Method - Read by Search query
   // http://localhost:5000/
@@ -9,12 +13,20 @@ const useSearchInventories = (email) => {
   useEffect(() => {
     const getItems = async () => {
       const url = `https://quiet-sierra-51150.herokuapp.com/my-items/?email=${email}`;
-      const { data } = await axios.get(url, {
-        headers: {
-          authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-        },
-      });
-      setInventory(data);
+      try {
+        const { data } = await axios.get(url, {
+          headers: {
+            authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+          },
+        });
+        setInventory(data);
+      } catch (error) {
+        console.log(error.message);
+        if (error.response.status === 401 || error.response.status === 403) {
+          signOut(auth);
+          navigate("/login");
+        }
+      }
     };
     getItems();
   }, [email]);
